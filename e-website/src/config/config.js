3ba12e79a09c4bde265369803e99810063957788
gnisoftsolutions <gnisoftsolutions@Piyush-Loona.local>
2024-03-11 11:11:31 +0530

    sonjeet code

---
+++ e-website/src/config/config.js
@@ -0,0 +1,17 @@
+// import site_logo from '../Assets/image/logo.png';
+// export const logo = site_logo;
+export const title = 'ecommerce';
+export const website = ''; 
+export const shortTitle = 'ecommerce';
+export const titleDescription = 'ecommerce-site';
+
+// export const api_url = 'https://weboadmin.payucoins.com/api/v1/';
+
+export const api_url = 'http://192.168.1.17:8000/api/';
+
+export const short_api_url = 'http://192.168.1.17:8000/tokenImage/';
+
+
+
+
+
